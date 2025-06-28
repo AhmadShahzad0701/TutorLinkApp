b1a47862413629420abc437a54cd56cc905e9215
Ahmad Shahzad <121196369+AhmadShahzad0701@users.noreply.github.com>
2025-06-28 13:19:31 +0500

    Folders Files
    Structure all folders and files

---
+++ TutorLinkApp/app/components/TutorCard.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const TutorCard = () => {
+  return (
+    <View>
+      <Text>TutorCard</Text>
+    </View>
+  )
+}
+
+export default TutorCard
---
+++ TutorLinkApp/app/screens/Signin.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const Signin = () => {
+  return (
+    <View>
+      <Text>Signin</Text>
+    </View>
+  )
+}
+
+export default Signin
