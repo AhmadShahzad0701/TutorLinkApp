44f27da9a4032b4c029031d643f894a67a495598
ZerlishBurhan <147303983+ZerlishBurhan@users.noreply.github.com>
2025-07-03 14:27:47 +0500

    Update _layout.tsx
    Update _layout.tsx

--- TutorLink/app/_layout.tsx
+++ TutorLink/app/_layout.tsx
@@ -7,7 +7,6 @@ export default function Layout() {
   return (
     <Drawer>
 
-      {/* Home */}
       <Drawer.Screen 
         name="index" 
         options={{ 
@@ -15,53 +14,35 @@ export default function Layout() {
           header: () => <Header />  
         }}  
       />
-
-      {/* Login */}
+      
       <Drawer.Screen 
         name="screens/Login" 
         options={{ 
           title: 'Login',
-          headerShown: false,
-          drawerItemStyle: { display: 'none' } 
+          headerShown: false        
         }}  
       />
 
-      {/* Sign Up */}
       <Drawer.Screen 
         name="screens/SignUp" 
         options={{ 
           title: 'Sign Up',
-          headerShown: false,
-          drawerItemStyle: { display: 'none' }
+          headerShown: false 
         }}  
       />
 
-      {/* Profile */}
       <Drawer.Screen
         name="screens/Profile"
-        options={{
-          title: 'Tutor Profile',
-          header: () => <Header />,
-        }}
-      />
-
-      {/* Edit Profile */}
+         options={{
+          title: 'Profile'
+          }}
+       />
+      
       <Drawer.Screen
         name="screens/EditProfile"
         options={{
           title: 'Edit Profile',
-          header: () => <Header />,
-          drawerItemStyle: { display: 'none' }
-        }}
-      />
-
-      {/* Optional: Create Profile page for tutors */}
-      <Drawer.Screen
-        name="screens/CreateProfile"
-        options={{
-          title: 'Create Profile',
-          header: () => <Header />,
-          drawerItemStyle: { display: 'none' },
+          drawerItemStyle: { display: 'none' } 
         }}
       />
     </Drawer>
